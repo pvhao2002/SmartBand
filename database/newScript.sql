ed9cceb927205c6a45760bfe3933d718dc38cfe8
Hao <hideonbush8405@gmail.com>
2024-04-18 14:21:29 +0700

    update bao cao and powerpoint

--- database/newScript.sql
+++ database/newScript.sql
@@ -1,191 +1,217 @@
-CREATE DATABASE shop_laptop;
-GO
-USE shop_laptop;
-GO
-CREATE TABLE users (
-    user_id INT IDENTITY PRIMARY KEY,
-    email NVARCHAR(255) NOT NULL,
-    password NVARCHAR(255) NOT NULL,
-    full_name NVARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL DEFAULT 'user',
-    status VARCHAR(50) NOT NULL DEFAULT 'inactive',
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE()
+drop database if exists `web-smartband`;
+create database if not exists `web-smartband`;
+use `web-smartband`;
+create table if not exists `otp` (
+    id int auto_increment primary key,
+    otp varchar(6) not null,
+    email varchar(50) not null,
+    created_at timestamp default current_timestamp
 );
-GO
-CREATE TABLE categories (
-    category_id INT IDENTITY PRIMARY KEY,
-    category_name NVARCHAR(255) NOT NULL,
-    description TEXT,
-    status VARCHAR(50) NOT NULL DEFAULT 'active',
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE()
+create table if not exists `role` (
+    role_id int auto_increment primary key,
+    role_code varchar(50) not null,
+    role_name varchar(50) not null
 );
-GO
-CREATE TABLE products (
-    product_id INT IDENTITY PRIMARY KEY,
-    product_name NVARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    description TEXT,
-    stock INT NOT NULL,
-    product_image NVARCHAR(2000) NOT NULL,
-    category_id INT NOT NULL,
-    status VARCHAR(50) NOT NULL DEFAULT 'active',
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+create table if not exists `permission` (
+    permission_id int auto_increment primary key,
+    permission_code varchar(50) not null,
+    permission_name varchar(50) not null,
+    role_id int not null,
+    foreign key (role_id) references role (role_id)
 );
-GO
-CREATE TABLE carts (
-    cart_id INT IDENTITY PRIMARY KEY,
-    user_id INT NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    total_quantity INT NOT NULL,
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+create table if not exists `users` (
+    user_id int auto_increment primary key,
+    email varchar(50) not null,
+    password varchar(255) not null,
+    phone varchar(15) not null,
+    full_name varchar(50) not null,
+    status enum ('active', 'inactive', 'block') default 'active',
+    point decimal(10, 2) default 0,
+    role_id int not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (role_id) references role (role_id)
 );
-GO
-CREATE TABLE cart_items (
-    cart_item_id INT IDENTITY PRIMARY KEY,
-    cart_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (cart_id) REFERENCES carts(cart_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+create table if not exists `address` (
+    address_id int auto_increment primary key,
+    user_id int,
+    address_name varchar(50) not null,
+    address_line varchar(500) not null,
+    city varchar(50) not null,
+    district varchar(50) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (user_id)
 );
-GO
-CREATE TABLE orders (
-    order_id INT IDENTITY PRIMARY KEY,
-    user_id INT NOT NULL,
-    shipping_address NVARCHAR(255) NOT NULL,
-    phone_number NVARCHAR(20) NOT NULL,
-    full_name NVARCHAR(255) NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    total_quantity INT NOT NULL,
-    status VARCHAR(50) DEFAULT 'pending',
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+create table if not exists `leave` (
+    leave_id int auto_increment primary key,
+    user_id int,
+    leave_date date not null,
+    leave_type enum ('FULL_DAY', 'HALF_DAY') not null,
+    message varchar(500) not null,
+    status enum ('pending', 'approved', 'rejected') default 'pending',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (user_id)
 );
-GO
-CREATE TABLE order_items (
-    order_item_id INT IDENTITY PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    CREATEd_at DATETIME DEFAULT GETDATE(),
-    updated_at DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+create table if not exists `blog_type` (
+    blog_type_id int auto_increment primary key,
+    blog_type_name varchar(50) not null,
+    status enum ('active', 'inactive') default 'active',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
 );
--- INSERT DATA FOR TABLE users
-INSERT INTO users (email, password, full_name, role, status)
-VALUES (
-        'admin@gmail.com',
-        '1234qwer',
-        'admin',
-        'admin',
-        'active'
-    );
-INSERT INTO users (
-        email,
-        password,
-        full_name,
-        role,
-        status
-    )
-VALUES (
-        'user1@example.com',
-        '1234qwer',
-        'User 1',
-        'user',
-        'active'
-    ),
-    (
-        'user2@example.com',
-        '1234qwer',
-        'User 2',
-        'user',
-        'active'
-    ),
-    (
-        'user3@example.com',
-        '1234qwer',
-        'User 3',
-        'user',
-        'active'
-    ),
-    (
-        'user4@example.com',
-        '1234qwer',
-        'User 4',
-        'user',
-        'active'
-    ),
-    (
-        'user5@example.com',
-        '1234qwer',
-        'User 5',
-        'user',
-        'active'
-    ),
-    (
-        'user6@example.com',
-        '1234qwer',
-        'User 6',
-        'user',
-        'active'
-    ),
-    (
-        'user7@example.com',
-        '1234qwer',
-        'User 7',
-        'user',
-        'active'
-    ),
-    (
-        'user8@example.com',
-        '1234qwer',
-        'User 8',
-        'user',
-        'active'
-    ),
-    (
-        'user9@example.com',
-        '1234qwer',
-        'User 9',
-        'user',
-        'active'
-    );
--- INSERT DATA FOR TABLE categories
-INSERT INTO categories (category_name, description, status)
-VALUES (
-        'Acer',
-        'Laptop acer',
-        'active'
-    ),
-    (
-        'Asus',
-        'Laptop asus',
-        'active'
-    ),
-    (
-        'Dell',
-        'Laptop dell',
-        'active'
-    ),
-    (
-        'HP',
-        'Laptop hp',
-        'active'
-    ),
-    (
-        'Lenovo',
-        'Laptop lenovo',
-        'active'
-    );
+create table if not exists `blog` (
+    blog_id int auto_increment primary key,
+    blog_type_id int,
+    subject varchar(255) not null,
+    content text not null,
+    image varchar(255) not null,
+    status enum ('active', 'inactive') default 'active',
+    created_by varchar(50) default 'admin',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (blog_type_id) references blog_type (blog_type_id)
+);
+create table if not exists `category` (
+    category_id int auto_increment primary key,
+    category_name varchar(50) not null,
+    description text,
+    status enum ('active', 'inactive') default 'active',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+);
+create table if not exists `brand` (
+    brand_id int auto_increment primary key,
+    brand_name varchar(50) not null,
+    status enum ('active', 'inactive') default 'active',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+);
+create table if not exists `product` (
+    product_id int auto_increment primary key,
+    category_id int,
+    brand_id int,
+    product_name varchar(50) not null,
+    price decimal(10, 2) not null,
+    description text,
+    status enum ('active', 'inactive') default 'active',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (category_id) references category (category_id),
+    foreign key (brand_id) references brand (brand_id)
+);
+create table if not exists `product_image` (
+    product_image_id int auto_increment primary key,
+    product_id int,
+    image varchar(255) not null,
+    foreign key (product_id) references product (product_id)
+);
+create table if not exists `product_info` (
+    product_info_id int auto_increment primary key,
+    product_id int,
+    usage_object varchar(50) not null,
+    dial_diameter varchar(50) not null,
+    strap_material varchar(50) not null,
+    glass_material varchar(50) not null,
+    water_resistant varchar(50) not null,
+    luminous_markets varchar(50) not null,
+    energy_source varchar(50) not null,
+    movement_type varchar(50) not null,
+    manufacturing_location varchar(50) not null,
+    foreign key (product_id) references product (product_id)
+);
+create table if not exists `product_voucher` (
+    product_voucher_id int auto_increment primary key,
+    product_id int,
+    voucher_code varchar(50) not null,
+    discount decimal(10, 2) not null,
+    start_date date,
+    end_date date,
+    status enum ('active', 'inactive') default 'active',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (product_id) references product (product_id)
+);
+create table if not exists `supplier` (
+    supplier_id int auto_increment primary key,
+    supplier_name varchar(50) not null,
+    email varchar(50) not null,
+    contact_person varchar(50) not null,
+    phone_number varchar(15) not null,
+    status enum ('active', 'inactive') default 'active'
+);
+create table if not exists `supplier_product` (
+    supplier_product_id int auto_increment primary key,
+    supplier_id int,
+    product_id int,
+    unit_price decimal(10, 2) not null,
+    quantity int not null,
+    status_order enum ('PAID', 'NOT_PAY') default 'NOT_PAY',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (supplier_id) references supplier (supplier_id),
+    foreign key (product_id) references product (product_id)
+);
+create table if not exists `order` (
+    order_id int auto_increment primary key,
+    user_id int,
+    shipping_address varchar(500) not null,
+    phone_number varchar(15) not null,
+    full_name varchar(50) not null,
+    total_price decimal(10, 2) not null,
+    total_discount decimal(10, 2) not null,
+    final_total_price decimal(10, 2) not null,
+    total_quantity int not null,
+    payment_method enum ('COD', 'VN_PAY', 'MOMO', 'CREDIT_CARD') default 'COD',
+    status_order enum (
+        'pending',
+        'processing',
+        'completed',
+        'canceled',
+        'refunded',
+        'done'
+    ) default 'pending',
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (user_id)
+);
+create table if not exists `order_item` (
+    order_item_id int auto_increment primary key,
+    order_id int,
+    product_id int,
+    quantity int not null,
+    unit_price decimal(10, 2) not null,
+    total_price decimal(10, 2) not null,
+    discount decimal(10, 2) not null,
+    final_total_price decimal(10, 2) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (order_id) references `order` (order_id),
+    foreign key (product_id) references product (product_id)
+);
+create table if not exists `cart` (
+    cart_id int auto_increment primary key,
+    user_id int,
+    total_price decimal(10, 2) not null,
+    total_quantity int not null,
+    total_discount decimal(10, 2) not null,
+    final_total_price decimal(10, 2) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (user_id)
+);
+create table if not exists `cart_item` (
+    cart_item_id int auto_increment primary key,
+    cart_id int,
+    product_id int,
+    quantity int not null,
+    unit_price decimal(10, 2) not null,
+    total_price decimal(10, 2) not null,
+    discount decimal(10, 2) not null,
+    final_total_price decimal(10, 2) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (cart_id) references cart (cart_id),
+    foreign key (product_id) references product (product_id)
+);
